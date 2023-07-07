c9e82940cacb30b2c53fb846df2222fc693035a5
OlgaYaroslavkina <131812667+OlgaYaroslavkina@users.noreply.github.com>
2023-07-07 11:34:10 +0300

    Зміни 07.07.23

--- HW09.py
+++ HW09.py
@@ -16,68 +16,77 @@ def input_error(func):
 
 
 @input_error
-def add_contact(name, phone_number):
-    contacts[name] = phone_number
-    return f"Контакт {name} з номером {phone_number} успішно доданий!"
+def add_contact(*args):
+    name = args[0]
+    phone = args[1]
+    contacts[name] = phone
+    return f"Контакт {name} з номером {phone} успішно доданий!"
 
 
 @input_error
-def find_contact(name):
-    return contacts[name]
+def change_contact(*args):
+    name = args[0]
+    phone = args[1]
+    if name in contacts:
+        contacts[name] = phone
+        return f"Номер телефону для контакту {name} успішно змінено на {phone}!"
+    else:
+        raise KeyError
 
 
 @input_error
-def update_contact(name, new_phone_number):
-    contacts[name] = new_phone_number
-    return f"Номер телефону для контакту {name} успішно оновлено!"
+def phone_command(*args):
+    name = args[0]
+    if name in contacts:
+        return f"Номер телефону для контакту {name}: {contacts[name]}"
+    else:
+        raise KeyError
 
 
 def show_all_contacts():
-    if not contacts:
-        return "Книга контактів порожня!"
-    result = "Книга контактів:\n"
-    for name, phone_number in contacts.items():
-        result += f"{name}: {phone_number}\n"
-    return result
-
-
-def handle_command(command):
-    if command.lower() == "hello":
-        return "How can I help you?"
-    elif command.lower().startswith("add"):
-        try:
-            _, name, phone_number = command.split(" ", 2)
-            return add_contact(name, phone_number)
-        except ValueError:
-            return "Необхідно ввести наступні параметри: add name phone"
-    elif command.lower().startswith("change"):
-        try:
-            _, name, phone_number = command.split(" ", 2)
-            return update_contact(name, phone_number)
-        except ValueError:
-            return "Необхідно ввести наступні параметри: change name new_phone"
-    elif command.lower().startswith("phone"):
-        try:
-            _, name = command.split(" ", 1)
-            return find_contact(name)
-        except ValueError:
-            return "Необхідно ввести наступні параметри: phone name"
-    elif command.lower() == "show all":
-        return show_all_contacts()
-    elif command.lower() in ["good bye", "close", "exit"]:
-        return "Good bye!"
+    if contacts:
+        result = "Список контактів:\n"
+        for name, phone in contacts.items():
+            result += f"{name}: {phone}\n"
+        return result
     else:
-        return "Невідома команда. Спробуйте ще раз."
+        return "Список контактів порожній."
+
+
+def hello_command(*args):
+    return "How can I help you?"
+
+
+def exit_command(*args):
+    return "Good bye!"
+
+
+COMMANDS = {
+    hello_command: ["hello"],
+    add_contact: ["add"],
+    change_contact: ["change"],
+    phone_command: ["phone"],
+    show_all_contacts: ["show all"],
+    exit_command: ["good bye", "close", "exit"],
+}
+
+
+@input_error
+def handle_command(command):
+    for cmd, keywords in COMMANDS.items():
+        for keyword in keywords:
+            if command.lower().startswith(keyword):
+                return cmd(*command.lower().replace(keyword, "").strip().split())
+    return "Невідома команда. Спробуйте ще раз."
 
 
 def main():
     print("Вітаємо у боті-асистенті!")
-
     while True:
         command = input("Введіть команду: ")
         response = handle_command(command)
         print(response)
-        if response == "Good bye!":
+        if isinstance(response, str) and response == "Good bye!":
             break
 
 
